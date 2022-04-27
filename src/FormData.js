08db9a31467e339ba5bb5804e8dc4f907818088d
kondrei <kondrei@gmail.com>
2022-04-27 22:28:58 +0300

    validate email and password

--- src/FormData.js
+++ src/FormData.js
@@ -1,6 +1,29 @@
 import "./styles.css";
+import dog from "./assets/dog.png"
+import eye from "./assets/icon.svg"
+import { useState } from "react";
+
 
 export default function FormData() {
+  const [showPassword, setShowPassword] = useState('password')
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+
+  function handleClickPassword() {
+    (showPassword === 'text') ? setShowPassword('password') : setShowPassword('text')
+  }
+
+  function validateEmail(e) {
+    let regex = /\S+@\S+\.\S+/
+    setEmail(e.target.value)
+    if (!e.target.value.match(regex)) {
+      e.target.className = 'error'
+    } else
+      e.target.className = ''
+  }
+
   return (
     <div className="app">
       <div className="container">
@@ -12,22 +35,9 @@ export default function FormData() {
               Create your account.
             </h1>
             <strong>Already a member? </strong>
-            <a href="#">Sign up</a>
+            <a href="/#">Sign up</a>
           </div>
-          <label htmlFor="firstName">First name</label>
-          <input
-            type="text"
-            name="firstName"
-            id="firstName"
-            placeholder="First name"
-          />
-          <label htmlFor="lastName">Last name</label>
-          <input
-            type="text"
-            name="lastName"
-            id="lastName"
-            placeholder="Last name"
-          />
+
 
           <label htmlFor="phone">Phone number</label>
           <input
@@ -37,22 +47,44 @@ export default function FormData() {
             placeholder="Phone number"
           />
 
-          <label htmlFor="password">Choose password</label>
+          <label htmlFor="email">E-mail</label>
           <input
-            type="password"
+            type="text"
+            name="email"
+            id="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => {
+              validateEmail(e)
+            }}
+          />
+
+          <span>
+            <label htmlFor="password">Choose password</label>
+            <img className="eye" src={eye} alt="show password" width={16} height={16}
+              onClick={handleClickPassword} />
+          </span>
+          <input
+            type={showPassword}
             name="password"
             id="password"
             placeholder="It’ll be our secret"
+            className={password === passwordConfirm ? '' : 'error'}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <label htmlFor="repeatPassword">Repeat password</label>
-          <input type="password" name="repeatPassword" id="repeatPassword" />
-          <div>
-            <input
-              type="checkbox"
-              name="tos"
-              id="tos"
-              placeholder="It’ll be our secret"
+          <span>
+            <label htmlFor="repeatPassword">Repeat password</label>
+            <img className="eye" src={eye} alt="show password" width={16} height={16}
+              onClick={handleClickPassword}
             />
+          </span>
+          <input type={showPassword} name="repeatPassword" id="repeatPassword"
+            value={passwordConfirm}
+            className={password === passwordConfirm ? '' : 'error'}
+            onChange={(e) => setPasswordConfirm(e.target.value)} />
+          <div>
+            <input type="checkbox" name="tos" id="tos" />
             <label htmlFor="tos">
               I consent to the Privacy Policy & Terms of Service.
             </label>
@@ -62,7 +94,6 @@ export default function FormData() {
               type="checkbox"
               name="newsletter"
               id="newsletter"
-              placeholder="It’ll be our secret"
             />
             <label htmlFor="newsletter">
               I want to receive stories for trends, inspiration, interior design
@@ -74,8 +105,8 @@ export default function FormData() {
         <div className="image">
           <img
             className="image"
-            src="https://drive.google.com/uc?id=1w-J2keSUFtGxQTNMoaVUZC7jqotqsWLB"
-            alt="dog is wathcing on your data"
+            src={dog}
+            alt="dog is watching on your data"
           />
         </div>
       </div>
